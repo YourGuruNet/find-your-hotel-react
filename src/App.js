b4fb0bc283f28501616e020bf3afdad927d59eda
arvis <info@yourgurunet.com>
2021-03-10 09:43:20 +0200

    clean

--- src/App.js
+++ src/App.js
@@ -1,24 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import axios from 'axios';
-// axios.defaults.baseURL = 'http://localhost:5001';
-
-// const responseBody = (response) => response.data;
-
-// const requests = {
-//   get: (url) => axios.get(url).then(responseBody),
-//   post: (url, body) => axios.post(url, body).then(responseBody),
-//   put: (url, body) => axios.put(url, body).then(responseBody),
-//   del: (url) => axios.delete(url).then(responseBody),
-// };
-
-// const ActivitiesApiCall = {
-//   list: () => requests.get('/activities'),
-//   details: (id) => requests.get(`/activities/${id}`),
-//   create: (activity) => requests.post('/activities', activity),
-//   update: (activity) => requests.put(`/activities/${activity.id}`, activity),
-//   delete: (id) => requests.del(`/activities/${id}`),
-// };
 
 const App = () => {
   const [items, setItems] = useState([]);
