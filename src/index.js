2dd5f694f53eb9ce46f21a0f7bc5ba14218d040c
arxarx123 <info@yourgurunet.com>
2022-09-17 13:24:03 +0300

    Update redux

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app/App";
-import reportWebVitals from "./reportWebVitals";
-import { createStore, applyMiddleware } from "redux";
+import { applyMiddleware } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import thunk from "redux-thunk";
 import { Provider } from "react-redux";
 import { appReducer } from "./modules/rootReducer";
 import { composeWithDevTools } from "redux-devtools-extension";
 
 const middleware = [thunk];
-const reduxStore = createStore(
-  appReducer,
+const reduxStore = configureStore(
+  { reducer: appReducer },
   composeWithDevTools(applyMiddleware(...middleware))
 );
 
@@ -22,8 +22,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
